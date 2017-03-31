bf3702e75275534a09120e8b03c22ec3ba036600
Matt Reishman <matt.reishman@Me.com>
2017-03-31 15:50:03 -0500

    1.0.0.1
    Sample File Upload

--- local/default/conf/config.php
+++ local/default/conf/config.php
@@ -8,6 +8,6 @@
 			'developmentTabEnabled' => 'false',
 			'enableDevBranchDownload' => 'false',
 			'watchList' => array(
-			'Matt Reishman' => array('WebsiteBase' =>  'mreishmandev.lan.goedekers.com','Folder' =>  '/var/www/html/','Website' =>  'mreishmandev.lan.goedekers.com'),'Blog Dev Mreishman' => array('WebsiteBase' =>  'mreishmandev.lan.goedekers.com','Folder' =>  '/var/www/html/blog/','Website' =>  'mreishmandev.lan.goedekers.com/blog'),'jpalanDev' => array('WebsiteBase' =>  'jpalandev.lan.goedekers.com','Folder' =>  '/var/www/html/','Website' =>  'jpalandev.lan.goedekers.com/blog'))
+			)
 		);
 	?>
