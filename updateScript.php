adb18ea64713b8cb7ac892e419ceb48e1f072dfe
Matt Reishman <mreishman@users.noreply.github.com>
2017-03-31 13:53:06 -0500

    Update and rename versionsCheckFile.php to updateScript.php

---
+++ updateScript.php
@@ -0,0 +1,11 @@
+<?php
+ 
+require_once("innerUpgradeStatus.php");
+
+  
+updateProgressFile("Finished Running Update Script", "", "updateProgressFileNext.php", "");
+updateProgressFile("Finished Running Update Script", "", "updateProgressFile.php", "");  
+  
+
+  
+?>
