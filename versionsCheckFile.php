43f75a7379a47002a5d72ec982db681ddce67538
Matt Reishman <mreishman@users.noreply.github.com>
2017-04-11 09:28:20 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -9,7 +9,7 @@ $versionCheckArray = array(
 		),
 		'1.1'		=> array(
 			'branchName'	=> '1.1Update',
-			'releaseNotes'  => '<ul><li>Features<ul><li>Added link to log-hog on watch servers (if installed)</li><li>Changes BG color of servers on master branch. </li><li>Auto-refresh / Pause auto refresh</li><li>Title for website is now a link to the website (www.website.com) - HTTPS:// is added by default</li><li>Re-adds ability to download dev branches</li></ul></li><li>Bug Fixes<ul><li>Fixed server add bug (non saved folders being removed when adding new server to watchlist)</li><li>Added error logic to ajax request</li><li>Opening and closing menu adjust width correctly</li><li>Navbar is fixed at top (doesn\'t scroll)</li></ul></ul>'
+			'releaseNotes'  => '<ul><li>Features<ul><li>Added link to log-hog on watch servers (if installed)</li><li>Changes BG color of servers on master branch. </li><li>Auto-refresh / Pause auto refresh</li><li>Title for website is now a link to the website (www.website.com) - HTTPS:// is added by default</li><li>Re-adds ability to download dev branches</li></ul></li><li>Bug Fixes<ul><li>Fixed server add bug (non saved folders being removed when adding new server to watchlist)</li><li>Added error logic to ajax request</li><li>Opening and closing menu adjust width correctly</li><li>Navbar is fixed at top (does not scroll)</li></ul></ul>'
 		),
 	)
 );
