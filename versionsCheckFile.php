efa4ca7281589e70ef4a9bc99751151f53819db2
Matt Reishman <mreishman@users.noreply.github.com>
2017-04-11 09:24:00 -0500

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -6,7 +6,11 @@ $versionCheckArray = array(
 		'1.0'	        => array(
 			'branchName'	=> '1.0Update',
 			'releaseNotes'	=> '<ul><li>Bug Fixes<ul><li>Adds check for header redirect</li><li>Fixed Unpause - on focus (if default paused) Bug</li><li>Adds remove directory / watch folder button</li></ul></li></ul>'
-		)
+		),
+		'1.1'		=> array(
+			'branchName'	=> '1.1Update',
+			'releaseNotes'  => '<ul><li>Features<ul><li>Added link to log-hog on watch servers (if installed)</li><li>Changes BG color of servers on master branch. </li><li>Auto-refresh / Pause auto refresh</li><li>Title for website is now a link to the website (www.website.com) - HTTPS:// is added by default</li><li>Re-adds ability to download dev branches</li></ul></li><li>Bug Fixes<ul><li>Fixed server add bug (non saved folders being removed when adding new server to watchlist)</li><li>Added error logic to ajax request</li><li>Opening and closing menu adjust width correctly</li><li>Navbar is fixed at top (doesn\'t scroll)</li></ul></ul>'
+		),
 	)
 );
 ?>
