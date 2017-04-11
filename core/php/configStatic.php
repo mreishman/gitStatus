da9d0380dbbfad52ec9d7c995ec4ceaf14501967
Matt Reishman <matt.reishman@Me.com>
2017-04-11 09:36:56 -0500

    1.1 Update

--- core/php/configStatic.php
+++ core/php/configStatic.php
@@ -4,8 +4,8 @@
 $configStatic = array(
   'version'   => '1.0.1',
   'lastCheck'   => '04-11-2017',
-  'newestVersion' => '1.0',
+  'newestVersion' => '1.1',
   'versionList' => array(
-  '1.0' => array('branchName' => '1.0Update','releaseNotes' => '<ul><li>Bug Fixes<ul><li>Adds check for header redirect</li><li>Fixed Unpause - on focus (if default paused) Bug</li><li>Adds remove directory / watch folder button</li></ul></li></ul>')
+  '1.0' => array('branchName' => '1.0Update','releaseNotes' => '<ul><li>Bug Fixes<ul><li>Adds check for header redirect</li><li>Fixed Unpause - on focus (if default paused) Bug</li><li>Adds remove directory / watch folder button</li></ul></li></ul>'),'1.1' => array('branchName' => '1.1Update','releaseNotes' => '<ul><li>Features<ul><li>Added link to log-hog on watch servers (if installed)</li><li>Changes BG color of servers on master branch. </li><li>Auto-refresh / Pause auto refresh</li><li>Title for website is now a link to the website (www.website.com) - HTTPS:// is added by default</li><li>Re-adds ability to download dev branches</li></ul></li><li>Bug Fixes<ul><li>Fixed server add bug (non saved folders being removed when adding new server to watchlist)</li><li>Added error logic to ajax request</li><li>Opening and closing menu adjust width correctly</li><li>Navbar is fixed at top (does not scroll)</li></ul></ul>')
   )
 );
