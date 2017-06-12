32def5e4670d55c04d9bf0af611802135679d509
Matt Reishman <mreishman@users.noreply.github.com>
2017-06-12 08:54:38 -0500

    Create updateScript.php

--- updateScript.php
+++ updateScript.php
@@ -1,11 +1,80 @@
 <?php
- 
+  
+$arrayOfFiles = array("about.php", "core_conf_config.php","core_html_changelog.html","core_html_popup.html","core_img_favicon.png","core_js_allPages.js","core_js_jscolor.js","core_js_main.js","core_php_functions_logHog.php","core_php_loadVars.php","core_php_saveFunctions_settingsSaveMain.php","core_php_templateFiles_sidebar.php","core_php_upgradeScripts_upgrade-1.2-1.3.php","index.php","local_default_template_theme.css","settings.php","settings-watchList.php","update.php");
+
 require_once("innerUpgradeStatus.php");
 
+if($innerUpdateProgress['currentFile'] < sizeOf($arrayOfFiles))
+{
+ 
+$currentFile = $arrayOfFiles[$innerUpdateProgress['currentFile']]; 
+$indexToExtracted = "update/downloads/updateFiles/extracted/";  
+$varToIndexDir = "";
+$countOfSlash = 0;
+while($countOfSlash < 20 && !file_exists($varToIndexDir."index.php"))
+{
+  $varToIndexDir .= "../";        
+}
+  
+//update innerUpgradeStatus file
+$newCount = $innerUpdateProgress['currentFile'] + 1;
+
+$currentFileArray = explode("_", $currentFile );  
+$sizeOfCurrentFileArray = sizeOf($currentFileArray);
+
+$nameOfFile = $currentFileArray[$sizeOfCurrentFileArray - 1];
+
+$directoryPath = "";
+  
+for($i = 0; $i < $sizeOfCurrentFileArray - 1; $i++)
+{
+  $directoryPath .= $currentFileArray[$i]."/"; 
+}
+ 
+$newFile = $directoryPath.$nameOfFile;
+$fileTransfer = file_get_contents($varToIndexDir.$indexToExtracted.$currentFile);
+file_put_contents($varToIndexDir.$newFile,$fileTransfer);  
+  
+$string = "Updating file ".$newCount." of ".sizeOf($arrayOfFiles). " - Updating this file -  ".$varToIndexDir.$newFile." - with this file - ".$varToIndexDir.$indexToExtracted.$currentFile; 
+  
+//update message for update  
+  
+updateProgressFile($string, "", "updateProgressFileNext.php", "");
+updateProgressFile($string, "", "updateProgressFile.php", "");   
+  
+$mainFileContents = file_get_contents("updateProgressLog.php");
+$mainFileContents = $string.$mainFileContents;	
+file_put_contents("updateProgressLog.php", $mainFileContents);	
+	
+  
+//update innerUpgradeStatus.php
+  
+$writtenTextTofile = "<?php
+	$"."innerUpdateProgress = array(
+  	'currentFile'   => '".$newCount."'
+	);
+	?>
+";
+
+
+
+file_put_contents($varToIndexDir.$indexToExtracted."innerUpgradeStatus.php", $writtenTextTofile);  
+
+if($currentFile == "index.php")
+{
+   require_once($varToIndexDir."core/php/upgradeScripts/upgrade-1.2-1.3.php");
+}
+  
+}
+else
+{
   
 updateProgressFile("Finished Running Update Script", "", "updateProgressFileNext.php", "");
 updateProgressFile("Finished Running Update Script", "", "updateProgressFile.php", "");  
   
+}
+
+
 
   
 ?>
