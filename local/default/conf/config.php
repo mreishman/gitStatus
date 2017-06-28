377d14db731a99b771e781b6ef88995da6fb8411
Matt Reishman <matt.reishman@Me.com>
2017-06-28 14:18:46 -0500

    Adds correct default settings to master

--- local/default/conf/config.php
+++ local/default/conf/config.php
@@ -8,7 +8,7 @@
 			'developmentTabEnabled' => 'false',
 			'enableDevBranchDownload' => 'false',
 			'watchList' => array(
-			'Name Of Server' => array('WebsiteBase' =>  'website.com','Folder' =>  '/var/www/html/','Website' =>  'website.com')
+			'Example Server' => array('WebsiteBase' =>  'website.com','Folder' =>  '/var/www/html/','Website' =>  'website.com','githubRepo' => 'githubRepo', 'groupInfo'=> 'Group')
 			)
 		);
 	?>
