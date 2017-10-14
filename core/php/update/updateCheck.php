17afa41396743e2f409030dd6c701006e78dfb09
Matt Reishman <matt.reishman@me.com>
2017-10-14 11:11:52 -0500

    Moved update check into php file

---
+++ core/php/update/updateCheck.php
@@ -0,0 +1,63 @@
+<?php
+
+$version = explode('.', $configStatic['version']);
+$newestVersion = explode('.', $configStatic['newestVersion']);
+
+$levelOfUpdate = 0; // 0 is no updated, 1 is minor update and 2 is major update
+$beta = false;
+
+$newestVersionCount = count($newestVersion);
+$versionCount = count($version);
+
+for($i = 0; $i < $newestVersionCount; $i++)
+{
+	if($i < $versionCount)
+	{
+		if($i == 0)
+		{
+			if($newestVersion[$i] > $version[$i])
+			{
+				$levelOfUpdate = 3;
+				break;
+			}
+			elseif($newestVersion[$i] < $version[$i])
+			{
+				$beta = true;
+				break;
+			}
+		}
+		elseif($i == 1)
+		{
+			if($newestVersion[$i] > $version[$i])
+			{
+				$levelOfUpdate = 2;
+				break;
+			}
+			elseif($newestVersion[$i] < $version[$i])
+			{
+				$beta = true;
+				break;
+			}
+		}
+		else
+		{
+			if($newestVersion[$i] > $version[$i])
+			{
+				$levelOfUpdate = 1;
+				break;
+			}
+			elseif($newestVersion[$i] < $version[$i])
+			{
+				$beta = true;
+				break;
+			}
+		}
+	}
+	else
+	{
+		$levelOfUpdate = 1;
+		break;
+	}
+}
+
+?>
--- settings-auth.php
+++ settings-auth.php
@@ -11,67 +11,7 @@ if(file_exists('local/layout.php'))
 require_once($baseUrl.'conf/config.php'); 
 require_once('core/conf/config.php');
 require_once('core/php/configStatic.php');  
-
-$version = explode('.', $configStatic['version']);
-$newestVersion = explode('.', $configStatic['newestVersion']);
-
-$levelOfUpdate = 0; // 0 is no updated, 1 is minor update and 2 is major update
-$beta = false;
-
-$newestVersionCount = count($newestVersion);
-$versionCount = count($version);
-
-for($i = 0; $i < $newestVersionCount; $i++)
-{
-	if($i < $versionCount)
-	{
-		if($i == 0)
-		{
-			if($newestVersion[$i] > $version[$i])
-			{
-				$levelOfUpdate = 3;
-				break;
-			}
-			elseif($newestVersion[$i] < $version[$i])
-			{
-				$beta = true;
-				break;
-			}
-		}
-		elseif($i == 1)
-		{
-			if($newestVersion[$i] > $version[$i])
-			{
-				$levelOfUpdate = 2;
-				break;
-			}
-			elseif($newestVersion[$i] < $version[$i])
-			{
-				$beta = true;
-				break;
-			}
-		}
-		else
-		{
-			if($newestVersion[$i] > $version[$i])
-			{
-				$levelOfUpdate = 1;
-				break;
-			}
-			elseif($newestVersion[$i] < $version[$i])
-			{
-				$beta = true;
-				break;
-			}
-		}
-	}
-	else
-	{
-		$levelOfUpdate = 1;
-		break;
-	}
-}
-
+require_once('core/php/update/updateCheck.php');
 
 #tmpvars
 $ldapCacheType = 'none';
