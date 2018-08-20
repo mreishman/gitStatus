08dbf4323fc6820a6fd6e4c02268c43a459a59d9
mreishman <matthew.reishman@goedekers.com>
2018-08-20 09:11:39 -0500

    more changes

--- settings.php
+++ settings.php
@@ -37,7 +37,7 @@ require_once('setup/setupProcessFile.php');
 	
 	<?php require_once('core/php/templateFiles/sidebar.php'); ?>
 	<?php require_once('core/php/templateFiles/header.php'); ?>
-	<div id="main">
+	<div id="main" style="overflow: auto; overflow-y: auto;" >
 		<div class="firstBoxDev">
 			<?php require_once('core/php/templateFiles/settingsMain.php');?>
 		</div>
@@ -68,58 +68,13 @@ require_once('setup/setupProcessFile.php');
 
 	function calcuateWidth()
 	{
-	var innerWidthWindow = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	if(document.getElementById("sidebar").style.width == '100px')
-	{
-		innerWidthWindow -= 103;
+		var left = "0px";
+		if(document.getElementById("sidebar").style.width == '100px')
+		{
+			left = "100px";
+		}
+		document.getElementById("main").style.left = left;
 	}
-	if(document.getElementById("sidebar").style.width == '100px')
-	{
-		document.getElementById("main").style.left = "103px";
-	}
-	else
-	{
-		document.getElementById("main").style.left = "0px";
-	}
-	var innerWidthWindowCalc = innerWidthWindow;
-	var innerWidthWindowCalcAdd = 0;
-	var numOfWindows = 0;
-	var elementWidth = 342;
-	while(innerWidthWindowCalc > elementWidth)
-	{
-		innerWidthWindowCalcAdd += elementWidth;
-		numOfWindows++;
-		if(numOfWindows == 1)
-		{
-			elementWidth = 542;
-		}
-		else if (numOfWindows == 2)
-		{
-			elementWidth = 342;
-		}
-		else if (numOfWindows == 3)
-		{
-			elementWidth = 542;
-		}
-		else if (numOfWindows == 4)
-		{
-			elementWidth = 500;
-		}
-		else if (numOfWindows == 5)
-		{
-			//change if adding more windows to settings.php
-			elementWidth = 9000000;
-		}
-		innerWidthWindowCalc -= elementWidth;
-	}
-	var windowWidthText = ((innerWidthWindowCalcAdd)+40)+"px";
-	document.getElementById("main").style.width = windowWidthText;
-	var remainingWidth = innerWidthWindow - ((innerWidthWindowCalcAdd)+40);
-	remainingWidth = remainingWidth / 2;
-	var windowWidthText = remainingWidth+"px";
-	document.getElementById("main").style.marginLeft = windowWidthText;
-	document.getElementById("main").style.paddingRight = windowWidthText;
-}
 
 	</script>
 	<script src="core/js/allPages.js?v=<?php echo $configStatic['version']; ?>"></script>
