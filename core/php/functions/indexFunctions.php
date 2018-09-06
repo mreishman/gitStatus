93d070a9e596794182717d16aef503e8333221fd
mreishman <matthew.reishman@goedekers.com>
2018-09-06 09:59:40 -0500

    start of 3.3

--- core/php/functions/indexFunctions.php
+++ core/php/functions/indexFunctions.php
@@ -134,15 +134,17 @@ function generateWindow($data = array(), $pollType)
 	$blockHTML .= "				</span>";
 	$blockHTML .= "				<img id=\"".$keyNoSpace."loadingSpinnerHeader\" class=\"loadingSpinnerHeader\" style=\"width: 20px; margin-bottom: -7px; display: none; margin-top: 3px; margin-left: 3px; margin-right: 1px;\" src=\"core/img/loading.gif\" >";
 	$blockHTML .= "				<a style=\"color: black;\" target=\"_blank\" href=\"https://".$website."\"><b>".$name."</b></a>";
-	$blockHTML .= "				<div ";
+	$blockHTML .= "			</div>";
+	$blockHTML .= "			<div style=\"background-color: white;\" >";
+	$blockHTML .= "			<img style=\"width: 20px;\" src=\"core/img/pin.png\">";
+	$blockHTML .= "				<div class=\"expandMenu\" onclick=\"dropdownShow('".$keyNoSpace."')\" ></div>";
+	$blockHTML .= "				<div  id=\"dropdown-".$keyNoSpace."\" class=\"dropdown-content\">";
+	$blockHTML .= "			    	<a  style=\"cursor: pointer; ";
 	if($pollType !== "1")
 	{
-		$blockHTML .= "style = \"display: none;\"";
+		$blockHTML .= " display: none; ";
 	}
-	$blockHTML .= "                  class=\"expandMenu\" onclick=\"dropdownShow('".$keyNoSpace."')\" ></div>";
-	$blockHTML .= "				<div  id=\"dropdown-".$keyNoSpace."\" class=\"dropdown-content\">";
-	$blockHTML .= "			    	<a style=\"cursor: pointer\" onclick=\"refreshAction('".$keyNoSpace."','inner');\" >Refresh</a>";
-	$blockHTML .= "					<span style=\"display: none;\" >";
+	$blockHTML .= "					 \" onclick=\"refreshAction('".$keyNoSpace."','inner');\" >Refresh</a>";
 	$blockHTML .= "			    	<div id=\"".$keyNoSpace."LogHogOuter\" style=\"display: none; cursor: pointer; width: 100%;\" >";
 	$blockHTML .= "						<a id=\"".$keyNoSpace."LogHogInner\" style=\"color: black;\" href=\"#\">Log-Hog</a>";
 	$blockHTML .= "					</div>";
@@ -152,11 +154,10 @@ function generateWindow($data = array(), $pollType)
 	$blockHTML .= "					<div id=\"".$keyNoSpace."SearchOuter\" style=\"display: none; cursor: pointer; width: 100%;\" >";
 	$blockHTML .= "						<a id=\"".$keyNoSpace."SearchInner\" style=\"color: black;\" href=\"#\">Search</a>";
 	$blockHTML .= "					</div>";
-	$blockHTML .= "					</span>";
 	$blockHTML .= "					<a id=\"".$keyNoSpace."errorMessageLink\" style=\"cursor: pointer; display: none;\">Error</a> ";
 	$blockHTML .= "					<a id=\"".$keyNoSpace."noticeMessageLink\" style=\"cursor: pointer; display: none;\">Notice</a> ";
 	$blockHTML .= "				</div>";
-	$blockHTML .= "			</div>";
+	$blockHTML .= " 		</div>";
 	$blockHTML .= "			<div class=\"devBoxContent\">";
 	$blockHTML .= "				<span style=\"display: ".$noticeMessageShow.";\" class=\"noticeMessage\" id=\"".$keyNoSpace."NoticeMessage\" >".$messageText."</span> ";
 	$blockHTML .= "				<b>";
