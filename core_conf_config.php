c70cf51a0dd34dbe0f0ca6c7654bcd50f004118a
Matt Reishman <mreishman@users.noreply.github.com>
2019-06-24 09:18:19 -0500

    Add files via upload

---
+++ core_conf_config.php
@@ -0,0 +1,92 @@
+<?php
+
+$defaultConfig = array(
+	'sliceSize'		=> 500,
+	'pollingRate'	=> 1,
+	'pollingRateBG'	=>	5,
+	'pausePoll'		=> 'false',
+	'pauseOnNotFocus' => 'true',
+	'pollType'		=> '2',
+	'disablePostRequestWithPostData'	=> 'false',
+	'maxCommits'	=>	100,
+	'autoCheckUpdate' => 'true',
+	'developmentTabEnabled' => 'false',
+	'enableDevBranchDownload' => 'false',
+	'enableSystemPrefShellOrPhp'	=> 'false',
+	'expSettingsAvail'	=> 'true',
+	'defaultViewBranch'	=> 'Standard',
+	'defaultExpandTab'	=>	'info',
+	'cacheEnabled'		=> 'true',
+	'dontNotifyVersion'	=> '0',
+	'onlyRefreshVisible'	=> 'true',
+	'defaultViewBranchCookie'	=> 'false',
+	'loginAuthType'	=> 'disabled',
+	'blockGitBranchName'	=>	'false',
+	'blockGitCommitDiff'	=>	'false',
+	'blockGitCommitHistory'	=>	'false',
+	'blockGitShowCommitStuff'	=>	'false',
+	'successVerifyNum'			=> 3,
+	'defaultBranchList'	=> 'master',
+	'errorAndColorArray'	=> array(
+		'error'	=> array(
+			'color'	=> 'C33',
+			'type'	=> 'default'
+			),
+		'master'	=> array(
+			'color'	=> '32CD32',
+			'type'	=> 'default'
+			),
+		'revert-'	=> array(
+			'color'	=> 'EE7600',
+			'type'	=> 'includes'
+			)
+		),
+	'errorAndColorAuthorArray'	=> array(
+		'error'	=> array(
+			'color'	=> 'C33',
+			'type'	=> 'default'
+			),
+		'dave'	=> array(
+			'color'	=> '32CD32',
+			'type'	=> 'default'
+			),
+		'matt'	=> array(
+			'color'	=> 'EE7600',
+			'type'	=> 'includes'
+			)
+		),
+	'errorAndColorComitteeArray'	=> array(
+		'error'	=> array(
+			'color'	=> 'C33',
+			'type'	=> 'default'
+			),
+		'dave'	=> array(
+			'color'	=> '32CD32',
+			'type'	=> 'default'
+			),
+		'matt'	=> array(
+			'color'	=> 'EE7600',
+			'type'	=> 'includes'
+			)
+		),
+	'branchColorFilter'	=> 'branchName',
+	'checkForIssueInBranchNameFilters'	=> array(
+				'Issue','issue','Issue_','issue_','Issue-','issue-','revert-'
+				),
+	'checkForIssueStartsWithNum'	=> 'true',
+	'checkForIssueEndsWithNum'	=> 'true',
+	'checkForIssueCustom'	=> 'true',
+	'checkForIssueInCommit'	=> 'true',
+	'messageTextEnabled'	=> 'false',
+	'onServerRemoveRemoveNotError'	=>	'false',
+	'messageText' => 'Example Message',
+	'enableBlockUntilDate'	=> 'false',
+	'defaultGroupViewOnLoad'	=> 'All',
+	'datePicker'	=> '07/04/2017',
+	'watchList' => array(
+			'Example Server' => array('WebsiteBase' =>  'website.com','urlHit' => 'location of file if not default', "Archive" => 'false')
+			),
+	'serverWatchList' => array(
+			'Example Server' => array('WebsiteBase' =>  'website.com','Folder' =>  '/var/www/html/', 'Website' =>  'website.com','githubRepo' => 'githubRepo', 'groupInfo'=> 'Group', 'urlHit' => 'location of file if not default', "type" => "local", "gitType" => "github", "Archive" => 'false')
+			)
+);
---
+++ core_php_templateFiles_settingsMain.php
@@ -0,0 +1,124 @@
+<?php
+$arrayOfGroups = array();
+$showTopBarOfGroups = false;
+$watchlistConfig = $config["watchList"];
+if($pollType === "2")
+{
+	$watchlistConfig = $config["serverWatchList"];
+}
+foreach ($watchlistConfig as $key => $value)
+{
+	if(isset($value['groupInfo']) && !is_null($value['groupInfo']) && ($value['groupInfo'] != "") )
+	{
+		$showTopBarOfGroups = true;
+		if(!in_array($value['groupInfo'], $arrayOfGroups))
+		{
+			array_push($arrayOfGroups, $value['groupInfo']);
+		}
+	}
+}
+array_push($arrayOfGroups, "All");
+?>
+<form id="settingsMainVars" action="core/php/saveFunctions/settingsSaveMain.php" method="post">
+	<div class="innerFirstDevBox"  >
+		<div class="devBoxTitle">
+			<b>Settings</b>
+			<?php if($setupProcess == "finished" || $setupProcess == "preStart"): ?>
+				<a class="buttonButton" onclick="saveAndVerifyMain('settingsMainVars');" >Save Changes</a>
+			<?php else: ?>
+				<button class="buttonButton" onclick="displayLoadingPopup();" >Save Changes</button>
+			<?php endif; ?>
+		</div>
+		<div class="devBoxContent">
+			<table width="100%">
+				<tr>
+					<td>
+						Check Update:
+					</td>
+					<td>
+						<select name="autoCheckUpdate">
+	  						<option <?php if($autoCheckUpdate == 'true'){echo "selected";} ?> value="true">Auto</option>
+	  						<option <?php if($autoCheckUpdate == 'false'){echo "selected";} ?> value="false">Manual</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Default View:
+					</td>
+					<td>
+						<select name="defaultViewBranch">
+							<option <?php if($defaultViewBranch == 'Minimized'){echo "selected";} ?> value="Minimized">Minimized</option>
+	  						<option <?php if($defaultViewBranch == 'Standard'){echo "selected";} ?> value="Standard">Standard</option>
+	  						<option <?php if($defaultViewBranch == 'Expanded'){echo "selected";} ?> value="Expanded">Expanded</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Default View Cookie:
+					</td>
+					<td>
+						<select name="defaultViewBranchCookie">
+	  						<option <?php if($defaultViewBranchCookie == 'true'){echo "selected";} ?> value="true">True</option>
+	  						<option <?php if($defaultViewBranchCookie == 'false'){echo "selected";} ?> value="false">False</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<p class="description" >Set default view by cookie, overrides above</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Enable Cache:
+					</td>
+					<td>
+						<select name="cacheEnabled">
+	  						<option <?php if($cacheEnabled == 'true'){echo "selected";} ?> value="true">All</option>
+	  						<option <?php if($cacheEnabled == 'read'){echo "selected";} ?> value="read">Read Only</option>
+	  						<option <?php if($cacheEnabled == 'write'){echo "selected";} ?> value="write">Write Only</option>
+	  						<option <?php if($cacheEnabled == 'false'){echo "selected";} ?> value="false">None</option>
+						</select>
+					</td>
+				</tr>
+				<?php if($showTopBarOfGroups): ?>
+				<tr>
+					<td>
+						Default Group
+					</td>
+					<td>
+						<select name="defaultGroupViewOnLoad">
+							<?php
+							sort($arrayOfGroups);
+							foreach ($arrayOfGroups as $key => $value):
+							?>
+							<option <?php if ($defaultGroupViewOnLoad == $value){echo "selected";}?> value="<?php echo $value; ?>"><?php echo $value; ?></option>
+							<?php
+							endforeach;
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<p class="description" >Default group visible on page load</p>
+					</td>
+				</tr>
+				<?php endif; ?>
+				<tr>
+					<td>
+						Default Expand Tab
+					</td>
+					<td>
+						<select name="defaultExpandTab">
+	  						<option <?php if($defaultExpandTab == 'info'){echo "selected";} ?> value="info">Info</option>
+	  						<option <?php if($defaultExpandTab == 'commits'){echo "selected";} ?> value="commits">Commits</option>
+						</select>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</form>
