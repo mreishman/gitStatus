6fa92d2beb04b21b7f23ba76a47e68d0131cab9e
Matt Reishman <mreishman@users.noreply.github.com>
2019-07-08 08:30:51 -0500

    Add files via upload

---
+++ core_php_functions_indexFunctions.php
@@ -0,0 +1,214 @@
+<?php
+function generateGroup($data = array())
+{
+	$group = "{{group}}";
+	$selected = "";
+	if(isset($data["group"]))
+	{
+		$group = $data["group"];
+	}
+	if(isset($data["defaultGroupViewOnLoad"]))
+	{
+		if($group === $data["defaultGroupViewOnLoad"])
+		{
+			$selected = "groupTabSelected";
+		}
+	}
+	$groupBlock =  "<div class=\"groupTabShadow\">";
+	$groupBlock .= "	<div data-group=\"".$group."\" class=\"groupTab ".$selected." \" id=\"Group".$group."\" onclick=\"showOrHideGroups(event, '".$group."');\" >";
+	$groupBlock .= 			$group;
+	$groupBlock .= "	</div>";
+	$groupBlock .= "</div>";
+	return $groupBlock;
+}
+
+function generateWindow($data = array(), $pollType)
+{
+
+	$groupInfo = "{{groupInfo}}";
+	$groupInfoStyle = "{{groupInfoStyle}}";
+	$backgroundColor = "{{backgroundColor}}";
+	$keyNoSpace = "{{keyNoSpace}}";
+	$website = "{{website}}";
+	$counter = "{{counter}}";
+	$name = "{{name}}";
+	$branchView = "{{branchView}}";
+	$branchViewClass = "{{branchView}}";
+	$branchViewClassTwo = "{{branchViewTwo}}";
+	$upArrow = "{{upArrow}}";
+	$downArrow = "{{downArrow}}";
+
+	$greenLED = "inline-block";
+	$yellowLED = "none";
+	$redLED = "none";
+	$noticeMessageShow = "none";
+	$messageText = "";
+	$showRefresh = "inline-block";
+
+	if(isset($data['groupInfo']))
+	{
+		$groupInfo = $data["groupInfo"];
+	}
+
+	if(isset($data['groupInfoStyle']))
+	{
+		$groupInfoStyle = $data["groupInfoStyle"];
+	}
+
+	if(isset($data['backgroundColor']))
+	{
+		$backgroundColor = $data["backgroundColor"];
+	}
+
+	if(isset($data['keyNoSpace']))
+	{
+		$keyNoSpace = "branchNameDevBox1".$data["keyNoSpace"];
+	}
+
+	if(isset($data['website']))
+	{
+		$website = $data["website"];
+	}
+
+	if(isset($data['counter']))
+	{
+		$counter = $data["counter"];
+	}
+
+	if(isset($data['name']))
+	{
+		$name = $data["name"];
+	}
+
+	if(isset($data['branchView']))
+	{
+		$branchView = $data["branchView"];
+	}
+
+	if(isset($data['greenLED']))
+	{
+		$greenLED = $data["greenLED"];
+	}
+
+	if(isset($data['yellowLED']))
+	{
+		$yellowLED = $data["yellowLED"];
+	}
+
+	if(isset($data['redLED']))
+	{
+		$redLED = $data["redLED"];
+	}
+
+	if(isset($data['noticeMessageShow']))
+	{
+		$noticeMessageShow = $data["noticeMessageShow"];
+	}
+
+	if(isset($data['messageText']))
+	{
+		$messageText = $data["messageText"];
+	}
+
+	if(isset($data['showRefresh']))
+	{
+		$showRefresh = $data["showRefresh"];
+	}
+
+	if($branchView === "Minimized")
+	{
+		$upArrow = "disabledArrow";
+		$downArrow = "";
+		$branchViewClass = "devBoxContentSecondary";
+		$branchViewClassTwo = "devBoxContentTertiary";
+	}
+	elseif($branchView === "Standard" || $branchView === "devBoxContentSecondary")
+	{
+		$upArrow = "";
+		$downArrow = "";
+		$branchViewClass = "devBoxContentSecondaryExpanded";
+		$branchViewClassTwo = "devBoxContentTertiary";
+	}
+	elseif($branchView === "Expanded" || $branchView === "devBoxContentSecondaryExpanded")
+	{
+		$upArrow = "";
+		$downArrow = "disabledArrow";
+		$branchViewClass = "devBoxContentSecondaryExpanded";
+		$branchViewClassTwo = "devBoxContentTertiaryExpanded";
+	}
+
+	$status = "<span style=\"display: none;\" id=\"".$keyNoSpace."Stats\"></span>";
+	$branchData = "<span id=\"".$keyNoSpace."\"><img style=\"width: 20px;\" src=\"core/img/loading.gif\"> Loading...</span>";
+
+	if(isset($data['status']))
+	{
+		$status = $data["status"];
+	}
+
+	if(isset($data['branchData']))
+	{
+		$branchData = $data["branchData"];
+	}
+
+	$blockHTML =  "	<div class=\"firstBoxDev ".$groupInfo." \"  ".$groupInfoStyle." >";
+	$blockHTML .= "		<div class=\"innerFirstDevBox\" id=\"innerFirstDevBox".$keyNoSpace."\"  ".$backgroundColor." >";
+	$blockHTML .= "			<div  class=\"devBoxTitle\" >";
+	$blockHTML .= "				<span id=\"".$keyNoSpace."warningSpanHeader\" class=\"warningSpanHeader\"  >";
+	$blockHTML .= "					<div class=\"led-red\" id=\"".$keyNoSpace."redwWarning\" style=\"display: inline-block; margin-bottom: -8px; display: ".$redLED." \"></div>";
+	$blockHTML .= "					<div class=\"led-yellow\" id=\"".$keyNoSpace."yellowWarning\" style=\"display: inline-block; margin-bottom: -8px; display: ".$yellowLED." \"></div>";
+	$blockHTML .= "					<div class=\"led-green\" id=\"".$keyNoSpace."greenNotice\" style=\"display: inline-block; margin-bottom: -8px; display: ".$greenLED." \"></div>";
+	$blockHTML .= "				</span>";
+	$blockHTML .= "				<img id=\"".$keyNoSpace."loadingSpinnerHeader\" class=\"loadingSpinnerHeader\" style=\"width: 20px; margin-bottom: -7px; display: none; margin-top: 3px; margin-left: 3px; margin-right: 1px;\" src=\"core/img/loading.gif\" >";
+	$blockHTML .= "				<a style=\"color: black;\" target=\"_blank\" href=\"https://".$website."\"><b>".$name."</b></a>";
+	$blockHTML .= "			</div>";
+	$blockHTML .= "			<div class=\"devBoxActions\" style=\"padding-left: 5px;  padding-right: 5px;\" >";
+	$blockHTML .= "			<img onclick=\"togglePinStatus('".$keyNoSpace."');\" id=\"".$keyNoSpace."Pin\" style=\"cursor: pointer; height: 18px;\" src=\"core/img/pin.png\">";
+	$blockHTML .= "			<img onclick=\"togglePinStatus('".$keyNoSpace."');\" id=\"".$keyNoSpace."PinPinned\" style=\"cursor: pointer; height: 18px; display: none;\" src=\"core/img/pinPinned.png\">";
+	$blockHTML .= "			<img onclick=\"toggleDetailBar(event, '".$keyNoSpace."');\" style=\"cursor: pointer; height: 18px;\" src=\"core/img/externalLink.png\">";
+	$blockHTML .= "			<img class=\"downArrow ".$downArrow."\" onclick=\"singleIncreaseView('".$keyNoSpace."');\" id=\"".$keyNoSpace."DownArrow\" style=\"cursor: pointer; height: 18px;\" src=\"core/img/downarrow.png\">";
+	$blockHTML .= "			<img class=\"upArrow ".$upArrow." \" onclick=\"singleDecreaseView('".$keyNoSpace."');\" id=\"".$keyNoSpace."UpArrow\" style=\"cursor: pointer; height: 18px;\" src=\"core/img/uparrow.png\">";
+	$blockHTML .= "				<div class=\"expandMenu\" onclick=\"dropdownShow('".$keyNoSpace."')\" ></div>";
+	$blockHTML .= "			    	<a  style=\"cursor: pointer;\" onclick=\"refreshAction('".$keyNoSpace."','inner');\" ><img style=\"height: 18px;\" src=\"core/img/Refresh2.png\"></a>";
+	$blockHTML .= "				<div  id=\"dropdown-".$keyNoSpace."\" class=\"dropdown-content\">";
+	$blockHTML .= "			    	<div id=\"".$keyNoSpace."LogHogOuter\" style=\"display: none; cursor: pointer; width: 100%;\" >";
+	$blockHTML .= "						<a id=\"".$keyNoSpace."LogHogInner\" style=\"color: black;\" target=\"_blank\" href=\"#\">Log-Hog</a>";
+	$blockHTML .= "					</div>";
+	$blockHTML .= "					<div id=\"".$keyNoSpace."MonitorOuter\" style=\"display: none; cursor: pointer; width: 100%;\" >";
+	$blockHTML .= "						<a id=\"".$keyNoSpace."MonitorInner\" style=\"color: black;\" target=\"_blank\" href=\"#\">Monitor</a>";
+	$blockHTML .= "					</div>";
+	$blockHTML .= "					<div id=\"".$keyNoSpace."SearchOuter\" style=\"display: none; cursor: pointer; width: 100%;\" >";
+	$blockHTML .= "						<a id=\"".$keyNoSpace."SearchInner\" style=\"color: black;\" target=\"_blank\" href=\"#\">Search</a>";
+	$blockHTML .= "					</div>";
+	$blockHTML .= "					<p style=\"border-bottom: 1px solid black;\" ></p>";
+	$blockHTML .= "					<a style=\"color: black;\" target=\"_blank\" href=\"https://".$website."\">Website</a>";
+	$blockHTML .= "					<div id=\"".$keyNoSpace."extraLinks\"></div> ";
+	$blockHTML .= "					<a id=\"".$keyNoSpace."errorMessageLink\" style=\"cursor: pointer; display: none;\">Error</a> ";
+	$blockHTML .= "					<a id=\"".$keyNoSpace."noticeMessageLink\" style=\"cursor: pointer; display: none;\">Notice</a> ";
+	$blockHTML .= "				</div>";
+	$blockHTML .= " 		</div>";
+	$blockHTML .= "			<div class=\"devBoxContent\">";
+	$blockHTML .= "				<span style=\"display: ".$noticeMessageShow.";\" class=\"noticeMessage\" id=\"".$keyNoSpace."NoticeMessage\" >".$messageText."</span> ";
+	$blockHTML .= "				<b>";
+	$blockHTML .= 					$branchData;
+	$blockHTML .= "				</b>";
+	$blockHTML .= "				<div class=\"".$branchViewClass."\">";
+	$blockHTML .= "					<span style=\"display: none;margin-top: 5px;\" id=\"".$keyNoSpace."UpdateOuter\">";
+	$blockHTML .= "						<b>Last Updated:</b>";
+	$blockHTML .= "						<span id=\"".$keyNoSpace."Update\"> --Pending-- </span>";
+	$blockHTML .= " 					<br>";
+	$blockHTML .= " 				</span>";
+	$blockHTML .= "					<div class=\"".$branchViewClassTwo."\">";
+	$blockHTML .= 					$status;
+	$blockHTML .= "					</div>";
+	$blockHTML .= "				</div>";
+	$blockHTML .= "			</div>";
+	$blockHTML .= "		</div>";
+	$blockHTML .= "	</div>";
+	$blockHTML .= "	<span style=\"display: none;\" id=\"".$keyNoSpace."BranchHistory\" ></span>";
+	return $blockHTML;
+}
+
+function clean_url($url) {
+    $parts = parse_url($url);
+    return $parts['path'];
+}
---
+++ core_php_templateFiles_watchListServer.php
@@ -0,0 +1,133 @@
+<form id="settingsMainWatch" action="core/php/saveFunctions/settingsSaveMain.php" method="post">
+	<div id="widthForWatchListSection" class="innerFirstDevBox" style="width: 500px;" >
+		<div class="devBoxTitle">
+			<b>Server Watch List</b>
+			<?php if($setupProcess == "finished" || $setupProcess == "preStart"): ?>
+				<a class="buttonButton" onclick="saveWatchList(false);" >Save Changes</a>
+			<?php else: ?>
+				<a class="buttonButton" onclick="saveWatchList(true);" >Save Changes</a>
+			<?php endif; ?>
+		</div>
+		<div class="devBoxContent">
+			<ul class="settingsUl">
+
+				<li><h2>Example:</h2></li>
+				<li class="watchFolderGroups">
+				<div style="border-bottom: 1px solid black; margin-bottom: 5px;">
+					<h2 style="margin: 5px;">Server 0</h2>
+				</div>
+				<span class="leftSpacingserverNames" > Name:</span> <input disabled="true" class='inputWidth300' type='text' value='Name you want to call website'>
+				<div style="border-bottom: 1px solid black; margin-bottom: 5px;">
+					<h4 style="margin: 5px;">General</h4>
+				</div>
+				<span class="leftSpacingserverNames" > Website Base:</span> <input disabled="true" class='inputWidth300' type='text' value='Base URL of website'>
+				<br>
+				<span class="leftSpacingserverNames" > Folder:</span> <input disabled="true" class='inputWidth300' type='text' value='Location of github repo on server'>
+				<br>
+				<span class="leftSpacingserverNames" > Website:</span> <input disabled="true" class='inputWidth300' type='text' value='Specific directory of website'>
+				<div style="border-bottom: 1px solid black; margin-bottom: 5px;">
+					<h4 style="margin: 5px;">Git</h4>
+				</div>
+				<span class="leftSpacingserverNames" > Git Repo:</span> <input disabled="true" class='inputWidth300' type='text' value='Name of your github repo: username/repo'>
+				<br>
+				<span>Put "auto" to automatically get the repo config on poll</span>
+				<br>
+ 				<span class="leftSpacingserverNames" > Branch List:</span> <input disabled="true" class='inputWidth300' type='text' value='Compare branches list example: master , develop'>
+				<br>
+				<span class="leftSpacingserverNames" > Git Type:</span>
+				<select disabled="true" class='inputWidth300' >
+ 					<option value="github" >github</option>
+ 					<option value="gitlab" >gitlab</option>
+ 				</select>
+ 				<br>
+ 				<span class="leftSpacingserverNames" > Custom Git:</span> <input disabled="true" class='inputWidth300' type='text' value='Custom url for git. Empty = default'>
+ 				<div style="border-bottom: 1px solid black; margin-bottom: 5px;">
+					<h4 style="margin: 5px;">Advanced</h4>
+				</div>
+				<span class="leftSpacingserverNames" > Group Info:</span> <input disabled="true" class='inputWidth300' type='text' value='Name of group'>
+				<br>
+				<span class="leftSpacingserverNames" > URL Hit:</span> <input disabled="true" class='inputWidth300' type='text' value='Location of file hit, blank = default'>
+				<br>
+				<span class="leftSpacingserverNames" > Type:</span>
+				<select disabled="true" class='inputWidth300' >
+ 					<option value="local" >Local</option>
+ 					<option value="external" >External</option>
+ 				</select>
+
+				</li>
+
+
+				<li><h2>Your Watch List: </h2></li>
+				<?php
+
+				$arrayKeys = array(
+					'WebsiteBase'		=> 'Website Base',
+					'Folder'			=> 'Folder',
+					'Website'			=> 'Website',
+					'githubRepo'		=> 'Git Repo',
+					'branchList'		=> 'Branch List',
+					'gitType'			=> 'Git Type',
+					'customGit'			=> 'Custom Git',
+					'groupInfo'			=> 'Group Info',
+					'urlHit'			=> 'URL Hit',
+					'type'				=> 'Type',
+					'Archive'			=> 'Archive'
+				);
+
+				$defaultArray = array(
+					'WebsiteBase'		=>  '',
+					'Folder'			=>  '',
+					'Website'			=>  '',
+					'githubRepo'		=>  '',
+					'branchList'		=>  'master',
+					'gitType'			=>	'',
+					'customGit'			=>  '',
+					'groupInfo'			=>  '',
+					'urlHit'			=>  '',
+					'type'				=>  '',
+					'Archive'			=>  'false'
+				);
+
+				$groups = array(
+					'General'	=>	array(
+						'WebsiteBase' 		=>  '',
+						'Folder' 			=>  '',
+						'Website' 			=>  ''
+					),
+					'Git'		=>	array(
+						'githubRepo' 		=>  '',
+						'branchList'		=>  'master',
+						'gitType'			=>	'github',
+						'customGit'			=>  ''
+					),
+					'Advanced'	=>	array(
+						'groupInfo' 		=>  '',
+						'urlHit' 			=>  '',
+						'type'				=>  '',
+						'Archive' 			=>  'false'
+					)
+				);
+				?>
+				<script type="text/javascript">
+					var arrayOfKeysNonEnc = <?php echo json_encode(array_keys($defaultArray)); ?>;
+					var numberOfSubRows = <?php echo count(array_keys($defaultArray)); ?>;
+					var countOfWatchList = <?php echo count(array_keys($config['serverWatchList'])); ?>;
+				</script>
+				<?php
+				$i = 0;
+				foreach($config['serverWatchList'] as $key => $item){
+					$i++;
+					echo generateWatchlistBlock($groups, $arrayKeys, $key, $item, $i);
+				} ?>
+				<div style="display: inline-block;" id="newRowLocationForWatchList"></div>
+			</ul>
+		</div>
+		<div id="hidden" style="display: none">
+			<span id="hiddenWatchlistFormBlank">
+				<?php echo generateWatchlistBlock($groups, $arrayKeys); ?>
+			</span>
+			<input id="numberOfRows" type="text" name="numberOfRows" value="<?php echo $i;?>">
+			<input id="watchListServer" type="text" name="watchListServer" value="true" >
+		</div>
+	</div>
+</form>
